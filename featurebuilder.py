c2bdb1f0bef74b7c3291ddd16c9d478f04406055
Maxim Kovalev <maxim.kovalev@west.cmu.edu>
2013-10-18 20:13:40 -0700

    DO NOT use pool.map. Just don't

--- featurebuilder.py
+++ featurebuilder.py
@@ -32,9 +32,9 @@ class FeatureBuilderOneDevice(object):
 	def build_pol_features(self, order):
 		self.pols = []
 		print "Building polynomal features (device id =", self.device_id, "order =", order, ")"
-		for v1, v2 in itertools.combinations_with_replacement(self.varrays, order):
-			print "v1:", len(v1), "v2:", len(v2)
-			self.pols.append(numpy.array(v1) * numpy.array(v2))
+		for vs in itertools.combinations_with_replacement(self.varrays, order):
+			self.pols.append(reduce(lambda x, y: x*numpy.array(y), vs, numpy.ones([1, len(vs[0])])))
+			print "Processing", len(vs), "features of", len(vs[0]), "readings done"
 		print len(self.pols), "polynomal features built (device id =", self.device_id, "order =", order, ")"
 
 	def process_one_device_parallel(self, all_var_modes = ["average", "maximum", "variance", "extremums", "average derivative", "average absolute derivative"], group_mode = "time", group_by = 20, pool_size = 6):
@@ -50,7 +50,7 @@ class FeatureBuilderOneDevice(object):
 def main():
 	fb = FeatureBuilderOneDevice(sensors = ["temp", "audio_p2p", "light", "humidity", "pressure", "motion"], device_id = 17000002, start_date_time = "2013-10-01 00:00:01", end_date_time = "2013-10-02 00:00:01")
 	fb.process_one_device()
-	fb.build_pol_features_parallel(order = 2)
+	fb.build_pol_features(order = 1)
 
 if __name__ == "__main__":
 	main()
