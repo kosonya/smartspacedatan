b72b574af29c21f13f89b3dfb9b52b5d632a1e78
Maxim Kovalev <maxim.kovalev@west.cmu.edu>
2013-10-21 15:33:03 -0700

    ...

--- v2/pca_load_and_plot.py
+++ v2/pca_load_and_plot.py
@@ -13,11 +13,12 @@ import timeprocessor
 group_by = 60
 
 
-def prepare_for_plotting(arr, threshold=3):
+def prepare_for_plotting(arr, threshold=10, debug = False):
 	avg = numpy.average(arr)
 	std = numpy.std(arr)
 	res = []
 	for x in arr:
+		if debug: print x, avg, std
 		if abs(x - avg) > std*threshold:
 			if x > avg:
 				res.append(avg+std*threshold)
@@ -25,13 +26,21 @@ def prepare_for_plotting(arr, threshold=3):
 				res.append(avg-std*threshold)
 		else:
 			res.append(x)
+		if debug: print "added:", res[-1], "\n"
 	return res
 
+def trim(arr, threshold=100):
+	res = [x if abs(x) < threshold else threshold for x in arr]
+	print arr, max(arr)
+	print res, max(res)
+	print ""
+	return res
 
 class MaxMins(object):
 	def __init__(self):
 		self.mins = []
 		self.maxes = []
+		self.summs = None
 	
 	def add_feats(self, feats):
 		if self.mins == []:
@@ -46,12 +55,24 @@ class MaxMins(object):
 			for i in xrange(len(self.maxes)):
 				if feats[i] > self.maxes[i]:
 					self.maxes[i] = feats[i]
+		if self.summs == None:
+			self.summs = numpy.array(feats)
+		else:
+			self.summs += feats
+
 
 	def get_maxes(self):
 		return self.maxes
 
 	def get_mins(self):
 		return self.mins
+
+	def get_avgs(self):
+		avgs = list(self.summs/self.summs.size)
+		print "avgs:", avgs
+		return avgs
+
+
 def main():
 	numpy.set_printoptions(precision=1, linewidth=284, threshold=40, edgeitems=13)
 	filename2 = sys.argv[1]
@@ -61,12 +82,12 @@ def main():
 	f.close()
 	print "Loading done"
 	_min_t, _max_t = new_dataloader.get_min_max_timestamps()
-	_min_t = timeprocessor.to_unixtime("2013-10-01 00:00:01")
-	_max_t = timeprocessor.to_unixtime("2013-10-03 00:00:01")
+	_min_t = timeprocessor.to_unixtime("2013-10-01 10:00:01")
+	_max_t = timeprocessor.to_unixtime("2013-10-02 00:00:01")
 	print "Minimum timestamp = %d (%s); maximum timestamp = %d (%s)" % (_min_t, timeprocessor.from_unixtime(_min_t), _max_t, timeprocessor.from_unixtime(_max_t))
 
 	devices = new_dataloader.fetch_all_macs()
-	#devices = ["17030002", "17030003"]
+	devices = ["17030002", "17030003"]
 	print "Devices:", devices
 
 
@@ -92,15 +113,14 @@ def main():
 				
 	raw_maxes = mm_raw.get_maxes()
 	pca_maxes = mm_pca.get_maxes()
+
 	raw_mins = mm_raw.get_mins()
 	pca_mins = mm_pca.get_mins()
-	plt.figure(1)
-	plt.subplot(211)
-	plt.bar(range(len(raw_maxes)), prepare_for_plotting(raw_maxes))
-	plt.bar(range(len(raw_mins)), prepare_for_plotting(raw_mins))
-	plt.subplot(212)
-	plt.bar(range(len(pca_maxes)), prepare_for_plotting(pca_maxes))
-	plt.bar(range(len(pca_mins)), prepare_for_plotting(pca_mins))
+
+	raw_avgs = mm_raw.get_avgs()
+
+	plt.bar(range(len(raw_avgs)), [x if (abs(x)) < 20000 else 20000 for x in raw_avgs])	
+
 	plt.show()
 
 if __name__ == "__main__":
