f7a488e255f58f9b427d5763b49184a610e589f2
Maxim Kovalev <maxim.kovalev@west.cmu.edu>
2013-10-24 16:02:43 -0700

    Cartesian plotter works

--- v2/pca_load_and_cartesian_plot.py
+++ v2/pca_load_and_cartesian_plot.py
@@ -71,66 +71,54 @@ def main():
 	f.close()
 	print "Loading done"
 	_min_t, _max_t = new_dataloader.get_min_max_timestamps()
-	_min_t = timeprocessor.to_unixtime("2013-10-01 10:00:01")
-	_max_t = timeprocessor.to_unixtime("2013-10-02 00:00:01")
+	_min_t = timeprocessor.to_unixtime("2013-10-02 10:00:01")
+	_max_t = timeprocessor.to_unixtime("2013-10-03 00:00:01")
 	print "Minimum timestamp = %d (%s); maximum timestamp = %d (%s)" % (_min_t, timeprocessor.from_unixtime(_min_t), _max_t, timeprocessor.from_unixtime(_max_t))
 
-	devices = new_dataloader.fetch_all_macs()
-	devices = ["17030002", "17030003"]
-	print "Devices:", devices
+	device  = "17030003" 
+
+	feats_used = range(5)
 
 
-	st_raw = DataStats()
-	st_pca = DataStats()
+	_TIME = []
+	_FEATS = []
+
+
 
 	for t_start in xrange(_min_t, _max_t+1, group_by):
 		t_end = t_start + group_by - 1
-		for device in devices:
-			print "Processing data from %s to %s (of %s through %s) from device %s" % (timeprocessor.from_unixtime(t_start), timeprocessor.from_unixtime(t_end), timeprocessor.from_unixtime(_min_t), timeprocessor.from_unixtime(_max_t), device)
+		print "Processing data from %s to %s (of %s through %s) from device %s" % (timeprocessor.from_unixtime(t_start), timeprocessor.from_unixtime(t_end), timeprocessor.from_unixtime(_min_t), timeprocessor.from_unixtime(_max_t), device)
 
-			count, data = new_dataloader.load_data_bundle(t_start, t_end, device)
-			if not data:
-				print "No data, skipped"
-			else:
-				uz_data = new_dataprocessor.unzip_data_bundle(data)
-				time_and_feats = new_dataprocessor.extract_all_features_from_sensors(uz_data)
-				print time_and_feats
-				_time, pols = new_dataprocessor.build_polynomial_features(time_and_feats, order=2)
-				st_raw.add(pols[0])
-				pca_feats = pnode.execute(pols)
-				st_pca.add(pca_feats[0])
-				
+		count, data = new_dataloader.load_data_bundle(t_start, t_end, device)
+		if not data:
+			_time = float(t_end + t_start) / 2
+			#_TIME.append(_time)
+			f = [0 for _ in xrange(len(feats_used))]
+			#_FEATS.append(f)
+			print "No data, skipped"
+		else:
+			uz_data = new_dataprocessor.unzip_data_bundle(data)
+			time_and_feats = new_dataprocessor.extract_all_features_from_sensors(uz_data)
+			print time_and_feats
+			_time, pols = new_dataprocessor.build_polynomial_features(time_and_feats, order=2)
+			pca_feats = pnode.execute(pols)
+			l = list(pca_feats[0])
+			_TIME.append(_time)
+			_FEATS.append([])
+			for i in xrange(len(feats_used)):
+				f = l[feats_used[i]]
+				_FEATS[-1].append(f)
+
 	plt.figure(1)
-
-	plt.subplot(221)
-	raw_maxes = st_raw.get_avgs()
-	plt.title("No PCA - averages")
-	plt.xlabel("Variable")
-	plt.bar(range(len(raw_maxes)), raw_maxes)
-
-	plt.subplot(222)
-	raw_stds = st_raw.get_stds()
-	plt.title("No PCA - stds")
-	plt.xlabel("Variable")
-	plt.bar(range(len(raw_stds)), raw_stds)
-
-
-	plt.subplot(223)
-	pca_maxes = st_pca.get_avgs()
-	plt.title("With PCA - averages")
-	plt.xlabel("Variable")
-	plt.bar(range(len(pca_maxes)), pca_maxes)
-
-
-	plt.subplot(224)
-	pca_stds = st_pca.get_stds()[5:100]
-	plt.title("With PCA - stds")
-	plt.xlabel("Variable")
-	plt.bar(range(len(pca_stds)), pca_stds)
-
+	i = 1
+	for _FEAT in zip(*_FEATS):
+		plt.subplot(len(feats_used), 1, i)
+		#print "time:", _TIME
+		#print "feat:", _FEAT
+		plt.plot(_TIME, _FEAT)
+		i += 1
 
 	plt.tight_layout()
-
 	plt.show()
 
 if __name__ == "__main__":
